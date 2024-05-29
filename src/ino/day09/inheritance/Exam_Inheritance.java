a9657237c0c0b4f4475c4d510f8666e40ca2ecd3
user1 <user1@DESKTOP-3NBJBC1>
2024-05-29 18:37:54 +0900

    [ADD] 24/05/29 자바 9일차  객체지향원칙 1, 캡슐화
    0. 클래스의 등장 배경
     - 변수 -> 배열 -> 구조체
     - private으로 필드를 접근제한하는 클래스로 구조체와 차이가 있음.
    1. 캡슐화 원칙
     - 필드 private
     - 메소드 public 우회 접근
    2. getter()/setter() 메소드 작성법

---
+++ src/ino/day09/inheritance/Exam_Inheritance.java
@@ -0,0 +1,15 @@
+package ino.day09.inheritance;
+
+public class Exam_Inheritance {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ColorPoint  cp = new ColorPoint(10, 23, "green");
+		cp.showColorPoint();
+
+//		ColorPoint  cp = new ColorPoint();
+//		cp.setX(5);
+//		cp.setY(7);
+//		cp.showPoint();
+	}
+}
